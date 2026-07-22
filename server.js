16339d5977af416fe7c678285c47749489ff5192
Mikyas~ <mikyasnegash2@gmail.com>
2026-07-22 18:49:51 +0300

    resetupping it for react real time chat app

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("Server running on port 3001");
+});
